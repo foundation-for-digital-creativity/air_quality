74af4fb8eeb05bae73cba562a2fd34fec448e430
inventorofthingies <49992909+inventorofthingies@users.noreply.github.com>
2019-06-04 06:43:34 +0100

    Update start_webserver.py

--- start_webserver.py
+++ start_webserver.py
@@ -1,4 +1,7 @@
 import inventthings.iot
-import inventthings.webserver 
+import inventthings.webserver
+
+# connect to WiFi
 wlan = inventthings.iot.wlan_connect()
+# start webserver on triot
 inventthings.webserver.run_server(wlan)
